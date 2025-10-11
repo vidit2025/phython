a3d3a86113692ff9da00e65e83c02e6e2d1b8809
vidit2025 <gamingbyvidit@gmail.com>
2025-10-11 11:59:31 +0530

    intial commit

---
+++ operators.py
@@ -0,0 +1,22 @@
+num1=45
+num2=3
+
+print("number 1", num1)
+print("number2", num2)
+print("addition :",num1+num2)
+print("difference :",num1-num2)
+print("product :",num1*num2)
+print("division :",num1/num2)
+print("floor division :",num1//num2)
+print("modules operation:",num1%num2)
+print("square :",num2**2)
+print("square :",num1**0.5)
+
+print("Equal ?" , num1==num2)
+print("number 1 grater?", num1>num2)
+print("number 2 greater?", num1<num2)
+print("Not Equal ?" , num1!=num2)
+
+
+result = num1/2+num2**2+10
+print("result",result)
---
+++ swapping.py
@@ -0,0 +1,11 @@
+x = input("Enter Value of x:") #5
+y = input("Enter Value of y:") #6
+
+# Swapping
+temp = x #5
+x = y #6
+y = temp  #5
+
+# Displaying results after swapping
+print("value of x after swapping", x)
+print("value of y after swapping", y)
