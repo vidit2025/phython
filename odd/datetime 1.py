731091cd5796d2546b10fd2cc524001a5a80d029
vidit2025 <gamingbyvidit@gmail.com>
2025-10-14 19:56:05 +0530

    initial commit

---
+++ odd/datetime 1.py
@@ -0,0 +1,10 @@
+import datetime
+current_time = datetime.datetime.now()
+
+
+print("Time now at greenwich meridian is :", end ="")
+print(current_time)
+#print(current_time.strftime("%A"))
+
+import calendar
+print("\n", calendar.calendar(2026))
