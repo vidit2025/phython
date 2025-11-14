fe9fa0db4682edc7480d779cbbb29505070d43a2
vidit2025 <gamingbyvidit@gmail.com>
2025-11-14 17:11:40 +0530

    initial commit

---
+++ square on a canvas.py
@@ -0,0 +1,19 @@
+import turtle
+
+#creating canvas 
+turtle.Screen().bgcolor("orange")
+
+sc = turtle.Screen()
+sc.setup(400, 300)
+
+turtle.title("Welcome to turtle window")
+
+#turtle object creation 
+board = turtle.Turtle()
+
+#creating a square 
+for i in range(4):
+          board.forward(100)
+          board.left(90)
+          i= i+1
+          
