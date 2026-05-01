7e68b3a698b3bfbb8b6c0486eb2352a2fd633ac9
vidit2025 <gamingbyvidit@gmail.com>
2026-05-01 19:39:22 +0530

    Add files via upload

---
+++ Asym[totic notation a-1.py
@@ -0,0 +1,12 @@
+#Program to show Constant time complexcity
+
+def printnumber(n):
+    iteration=0
+    print("The number entered is", n)
+    iteration+=1
+    print("Total iterations dobe by the code is", "\n")
+
+
+printnumber(10)
+printnumber(20)
+print("\n with any 'n' the time taken by our code won't change")
