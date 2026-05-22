bed4822b8d5114e0e123354c8938027c871eca81
vidit2025 <gamingbyvidit@gmail.com>
2026-05-22 19:52:54 +0530

    Add files via upload

---
+++ ImplementBitWise.py
@@ -0,0 +1,17 @@
+
+num1 = 10
+num2 = 4
+print(~num1, ~num2)
+
+num1 = 10 #1010
+num2 = 4 #0100
+
+print("num1 & num2 are:", num1 & num2)
+print("num1 | num2 are:",num1 | num2)
+print("num1 ^ num2 are:", num1 ^ num2)
+#Using right shift operator
+print("num1 >> 1 are:", num1 >>1)
+print("num2 >> 1 are:", num2 >>1)
+#Using left shift operator
+print("num1 << 1 are:", num1 <<1)
+print("num2 << 1 are:", num2 <<1)
