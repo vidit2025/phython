e211f2e8573fb4a111cc71669ba2d7e87c7c2de3
vidit2025 <gamingbyvidit@gmail.com>
2026-08-01 20:24:52 +0530

    Update power map.py

---
+++ power map.py
@@ -0,0 +1,11 @@
+# POWER MAP
+#Topics : power set | binary mask | bit probe | Subset enumeration| bit difference
+
+items = ["A", "B", "C"]
+n = len(items)
+total = 2 ** n
+
+print("=== Power Map ===")
+print("Items:", items)
+print("Elements:", n, " Total subsets: 2 ^", n, "=", total)
+print()
